4dce0b442b95151649a98cc63600c3862caa2dd9
Victor Antunez <victorac94@hotmail.com>
2018-11-04 15:48:46 +0100

    Updates

---
+++ precache-manifest.805f6197685a045bad42a2c841c2f108.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "440cb82e0fb936e48214",
+    "url": "/TO-DO-APP/static/js/runtime~main.440cb82e.js"
+  },
+  {
+    "revision": "521aeef11798085a56bd",
+    "url": "/TO-DO-APP/static/js/main.521aeef1.chunk.js"
+  },
+  {
+    "revision": "290559f1c7540c57a817",
+    "url": "/TO-DO-APP/static/js/1.290559f1.chunk.js"
+  },
+  {
+    "revision": "521aeef11798085a56bd",
+    "url": "/TO-DO-APP/static/css/main.2e33b5c2.chunk.css"
+  },
+  {
+    "revision": "e6501eb89199225517b2b4137b99568b",
+    "url": "/TO-DO-APP/index.html"
+  }
+];
